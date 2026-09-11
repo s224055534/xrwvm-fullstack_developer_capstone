80d079d9553db3389b0e490377f78574dd7a9923
s224055534 <ppngodidi322@gmail.com>
2026-09-11 08:43:06 -0400

    changes-v1.3

--- server/database/app.js
+++ server/database/app.js
@@ -17,6 +17,7 @@ mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 const Reviews = require('./review');
 
 const Dealerships = require('./dealership');
+const dealership = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
@@ -49,7 +50,9 @@ app.get('/fetchReviews', async (req, res) => {
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
   try {
-    const documents = await Reviews.find({dealership: req.params.id});
+    const documents = await Reviews.find({
+        dealership: req.params.id
+    });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
@@ -58,17 +61,39 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    //Write your code here
+    try {
+        const dealers = await Dealerships.find();
+        res.json(dealers)
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships' });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    //Write your code here
+    try {
+        const dealers = await Dealerships.find({
+            state: req.params.state
+        });
+        res.json(dealers);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching dealerships'});
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    //Write your code here
+    try {
+        const dealer = await Dealerships.find({
+            id: parseInt(req.params.id)
+        });
+        res.json(dealer);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching dealership'});
+    }
 });
 
 //Express route to insert review
